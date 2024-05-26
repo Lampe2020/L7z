103948c8ea1c986cee9ad4ce92022f052551a436
Lampe2020 <kontakt@lampe2020.de>
2024-05-26 14:31:40 +0200

    Replace sample main.py with l7z.py

---
+++ l7z.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+# L7z - the 7-zip GUI for *NIX
+
+
+'''
+
+import sys, os
+if __name__ != '__main__':
+    print('\n\nL7z is not intended to be imported!\n\n', file=sys.stderr)
+    raise ImportError('L7z is not intended to be imported!')
+
+...#TODO: Implement the GUI
