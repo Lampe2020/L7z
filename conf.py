791409167d36b1851712b054b9831f5d151ac3c7
Lampe2020 <kontakt@lampe2020.de>
2024-06-07 11:30:52 +0200

    Add partial menu bar FIXME: Doesn't show up when set to native menu bar?

---
+++ conf.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+L7z config handler
+"""
+
+import configparser, os
+config:configparser.ConfigParser = configparser.ConfigParser()
+configpath:str = os.path.expanduser('~/.config/l7z.conf')
+config.read(configpath)
+
+def get_sys_lang() -> str:
+    """
+    Retrieves the default language.
+    :return:
+    """
+    try:
+        return os.environ['LANG']
+    except OSError:
+        return 'en'
+
+if not 'L7z' in config.sections():
+    config['L7z'] = {
+        'lang': get_sys_lang()[:2], # For example 'sv_SE.UTF-8' → 'sv'
+        'native_menubar': 'off'
+    }
+    os.makedirs(os.path.dirname(configpath), exist_ok=True) # Create the config directory if it doesn't already exist
+    with open(configpath, 'w') as conf_file:
+        config.write(conf_file)
+
+def get(conf_name:str, section:str='L7z', fallback:str='') -> str:
+    """
+    Retrieve a config value
+    :param conf_name:
+    :param section:
+    :return:
+    """
+    try:
+        return config[section][conf_name]
+    except:
+        return fallback
+
+def set(conf_name:str, section:str='L7z', value:any='') -> bool:
+    """
+    Change a config value
+    :param conf_name:
+    :param value:
+    :param section:
+    :return:
+    """
+    try:
+        config[section][conf_name] = value
+        with open(configpath, 'w') as conf_file:
+            config.write(conf_file)
+        return True
+    except:
+        return False
+
+def getbool(conf_name:str, section:str='L7z', fallback:bool=False) -> bool:
+    """
+    Retrieve a config value as a boolean
+    :param conf_name:
+    :param section:
+    :param fallback:
+    :return:
+    """
+    try:
+        return config.getboolean(section, conf_name)
+    except:
+        return fallback
+
+def getint(conf_name:str, section:str='L7z', fallback:int=0) -> int:
+    """
+    Retrieve a confi value as an int
+    :param conf_name:
+    :param section:
+    :param fallback:
+    :return:
+    """
+    try:
+        return config.getint(section, conf_name)
+    except:
+        return fallback
+
+def getfloat(conf_name:str, section:str='L7z', fallback:float=0) -> float:
+    """
+    Retrieve a confi value as a float
+    :param conf_name:
+    :param section:
+    :param fallback:
+    :return:
+    """
+    try:
+        return config.getfloat(section, conf_name)
+    except:
+        return fallback
+
+__all__ = ['get', 'set', 'getbool', 'getint', 'getfloat']
+
+if __name__ == '__main__':
+    """Create a new config or repair the existing one"""
+    ... #TODO: Implement this
---
+++ languages.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+A simple little language module based on gettext, for the use specifically in L7z.
+Usage:
+```
+>>> from languages import *
+>>> lang_en.install()
+>>> # Do whatever you want. Then, if you want to switch to German, you can do:
+>>> lang_de.install()   # and boom, now you get the German strings.
+```
+"""
+
+import gettext, os, sys, conf
+
+APP_NAME:str = 'l7z'
+LOCALE_DIR:str = os.path.join(os.path.dirname(__file__), 'locales')
+gettext.install(APP_NAME, LOCALE_DIR)
+
+lang_en = gettext.translation(APP_NAME, languages=['en'], fallback=True)
+print(gettext.find(domain=APP_NAME, localedir=LOCALE_DIR, languages=['de']))
+lang_de = gettext.translation(APP_NAME, localedir=LOCALE_DIR, languages=['de'])
+lang_sv = gettext.translation(APP_NAME, localedir=LOCALE_DIR, languages=['sv'])
+
+match conf.get('lang'):
+    case 'en':
+        lang_en.install()
+    case 'de':
+        lang_de.install()
+    case 'sv':
+        lang_sv.install()
+    case language:
+        sys_lang:str = conf.get_sys_lang()[:2]
+        try:
+            gettext.translation(APP_NAME, localedir=LOCALE_DIR, languages=[sys_lang])
+            print(_('Unsupported lang in conf: {lang}, reverting to {sys_lang}.').format(
+                lang=repr(language),
+                sys_lang=repr(sys_lang)
+            ), file=sys.stderr)
+            conf.set('lang', sys_lang)
+        except:
+            lang_en.install()
+            print(
+                f"Unsupported lang in conf and unsupported system lang ({(lang, sys_lang)=}, reverting to 'en'",
+                file=sys.stderr
+            )
+            conf.set('lang', value='en')
+
+__all__ = list(obj for obj in locals() if obj.startswith('lang_'))
