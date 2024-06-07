dce51ab2cb9025cc66296fec4e37cbbfb1a9990f
Lampe2020 <kontakt@lampe2020.de>
2024-06-07 09:58:56 +0200

    Add the icon

---
+++ l7z.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+# L7z - the 7-zip GUI for *NIX
+
+TODO: Write short description and usage help, to be output when help is requested from the terminal
+'''
+
+import sys, os, conf
+
+# Initialize the translation right-away so even the no-import message can be localized.
+from languages import *
+
+if __name__ != '__main__':
+    msg:str = _("L7z is not intended to be imported!")
+    print(f'\n\n{msg}\n\n', file=sys.stderr)
+    raise ImportError(msg)
+
+INSTALL_DIR:str = os.path.dirname(__file__)
+
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QIcon
+
+l7z_app:QApplication = QApplication(sys.argv)
+
+class L7z_GUI(QMainWindow):
+    """The main GUI class"""
+    def __init__(self):
+        """Initialize the main GUI"""
+        super().__init__()
+        self.setWindowTitle(_('7-zip • Unofficial GUI (WIP!)'))
+        #self.setWindowIcon()
+        ... #TODO:
+        self.setWindowIcon(QIcon(os.path.join(INSTALL_DIR, 'icons', '7-zip.png')))
+        self.menubar = self.menuBar()
+
+#debug
+print(_('Starting L7z on {platform}…').format(platform=sys.platform))
+
+main_window:L7z_GUI = L7z_GUI()
+main_window.show()
+l7z_app.exec()
